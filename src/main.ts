8253072da71e8cb7191c03dfa8f24a4052febe46
Linal180 <amirnisar75@gmail.com>
2023-02-28 12:49:30 +0500

    added cors allow

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bodyParser: true,
+    cors: true 
+  });
   const port = process.env.PORT || 3001;
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
