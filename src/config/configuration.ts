0090a6c7b175ec30d2121b023524b867b38a9a2d
Linal180 <amirnisar75@gmail.com>
2023-02-28 11:53:11 +0500

    added local, staging, prod enviorments

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -15,12 +15,12 @@ export default () => {
     };
   } else {
     database = {
-      host: process.env.DATABASE_HOST || 'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
+      host:  'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
       port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      type: process.env.DATABASE_TYPE || 'postgres',
-      username: process.env.DATABASE_USERNAME || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'stagingeducation#123',
-      database: process.env.DATABASE_NAME || 'education-platform',
+      type:  'postgres',
+      username:  'postgres',
+      password:  'stagingeducation#123',
+      database: 'education-platform',
       synchronize: false,
       migrationsRun: true,
       autoLoadEntities: true,
