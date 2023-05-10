22a19a9579a2cee6efe7746321f76e962f2c832e
Linal180 <amirnisar75@gmail.com>
2023-05-10 18:29:11 +0500

    deloyment

--- typeorm-data-source.ts
+++ typeorm-data-source.ts
@@ -1,12 +1,12 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
-const devPGOptions = {
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'password',
-}
+// const devPGOptions = {
+//   host: process.env.DATABASE_HOST || 'localhost',
+//   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+//   username: process.env.POSTGRES_USER || 'postgres',
+//   password: process.env.DATABASE_PASSWORD || 'password',
+// }
 
 const stagPGOptions = {
   host: process.env.DATABASE_HOST || 'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
@@ -15,12 +15,12 @@ const stagPGOptions = {
   password: process.env.DATABASE_PASSWORD || 'stagingeducation#123',
 }
 
-const prodPGOptions = {
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'password',
-};
+// const prodPGOptions = {
+//   host: process.env.DATABASE_HOST || 'localhost',
+//   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+//   username: process.env.POSTGRES_USER || 'postgres',
+//   password: process.env.DATABASE_PASSWORD || 'password',
+// };
 
 let options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
@@ -31,13 +31,13 @@ let options: DataSourceOptions & SeederOptions = {
 };
 
 
-if (process.env.NODE_ENV === 'local') {
-  options = { ...options, ...devPGOptions }
-} else if (process.env.NODE_ENV === 'staging') {
+// if (process.env.NODE_ENV === 'local') {
+//   options = { ...options, ...devPGOptions }
+// } else if (process.env.NODE_ENV === 'staging') {
+//   options = { ...options, ...stagPGOptions }
+// } else {
   options = { ...options, ...stagPGOptions }
-} else {
-  options = { ...options, ...prodPGOptions }
-}
+// }
 
 const dataSource = new DataSource(options)
 export default dataSource;
