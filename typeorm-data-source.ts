15db819b7b8bbe5aa025dd6ed63e148349b2ec80
Linal180 <amirnisar75@gmail.com>
2023-05-25 16:17:04 +0500

    local config

--- typeorm-data-source.ts
+++ typeorm-data-source.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
+
 const devPGOptions = {
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
@@ -8,14 +9,14 @@ const devPGOptions = {
   password: process.env.DATABASE_PASSWORD || 'password',
 }
 
-const stagPGOptions = {
-  host: process.env.DATABASE_HOST || 'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'password',
-}
+// const stagPGOptions = {
+//   host: process.env.DATABASE_HOST || 'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
+//   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+//   username: process.env.POSTGRES_USER || 'postgres',
+//   password: process.env.DATABASE_PASSWORD || 'stagingeducation#123',
+// }
 
-// const prodPGOptions = {
+// const prodPGOptions = { 
 //   host: process.env.DATABASE_HOST || 'localhost',
 //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
 //   username: process.env.POSTGRES_USER || 'postgres',
@@ -24,21 +25,14 @@ const stagPGOptions = {
 
 let options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
-  database: process.env.DATABASE_NAME || 'education-platform',
+  database:  process.env.DATABASE_NAME || 'education-platform-2',
   entities: ['src/**/**/*.entity.{ts,js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   seeds: ['src/seeders/*.seeder{.ts,.js}'],
 };
 
 
-if (process.env.NODE_ENV === 'local') {
-  options = { ...options, ...devPGOptions }
-} else if (process.env.NODE_ENV === 'staging') {
-  options = { ...options, ...stagPGOptions }
-} else {
-  console.log("devPGOptions", devPGOptions)
-  options = { ...options, ...devPGOptions }
-}
+options = { ...options, ...devPGOptions }
 
 const dataSource = new DataSource(options)
 export default dataSource;
