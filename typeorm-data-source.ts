8a1f2ddb8e6a5d18963f86ffc0c136e587dc2f77
Linal180 <amirnisar75@gmail.com>
2023-05-26 15:59:31 +0500

    configurations settings

--- typeorm-data-source.ts
+++ typeorm-data-source.ts
@@ -2,12 +2,12 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
 
-// const devPGOptions = {
-//   host: process.env.DATABASE_HOST || 'localhost',
-//   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-//   username: process.env.POSTGRES_USER || 'postgres',
-//   password: process.env.DATABASE_PASSWORD || 'password',
-// }
+const devPGOptions = {
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'password',
+}
 
 const stagPGOptions = {
   host: process.env.DATABASE_HOST || 'staging-education.ctywplziivm7.us-east-1.rds.amazonaws.com',
@@ -32,7 +32,11 @@ let options: DataSourceOptions & SeederOptions = {
 };
 
 
-options = { ...options, ...stagPGOptions }
+if (process.env.NODE_ENV === 'local') {
+  options = { ...options, ...devPGOptions }
+} else {
+  options = { ...options, ...stagPGOptions }
+}
 
 const dataSource = new DataSource(options)
-export default dataSource;
+export default dataSource
