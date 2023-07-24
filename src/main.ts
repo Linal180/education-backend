d9a54f81d9fd8d7124d874c621805d7bec2dbc1e
Linal180 <amirnisar75@gmail.com>
2023-07-24 18:18:11 +0500

    CORS code reversed

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { RedisIoAdapter } from './adapters/redis-io.adapter';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
-  const redisIoAdapter = new RedisIoAdapter(app, configService);
-  app.useWebSocketAdapter(redisIoAdapter);
-  await redisIoAdapter.connectToRedis();
-
-  // Allow requests from all origins (including Vercel domain)
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Adjust the allowed methods as needed
-    allowedHeaders: ['Content-Type', 'Authorization'], // Adjust the allowed headers as needed
-    credentials: true, // Allow credentials like cookies, if applicable
-    preflightContinue: false, // Set this to true if you want to handle preflight requests explicitly
-    optionsSuccessStatus: 200, // Set the response status code for successful OPTIONS requests
-  });
-
-  await app.listen(process.env.PORT || 3001);
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3001;
+  app.enableCors(); // Enable CORS for all requests
+  await app.listen(port);
 }
 bootstrap();
