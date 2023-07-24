999f5391637e4bc6acc20433893850927c1f4ec0
Linal180 <amirnisar75@gmail.com>
2023-07-24 17:49:42 +0500

    CORS issue fixed

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,20 @@ import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService)
-  const redisIoAdapter = new RedisIoAdapter(app, configService)
+  const configService = app.get(ConfigService);
+  const redisIoAdapter = new RedisIoAdapter(app, configService);
   app.useWebSocketAdapter(redisIoAdapter);
   await redisIoAdapter.connectToRedis();
-  // app.enableCors(); // Enable CORS for all requests
+  app.enableCors({
+    origin: true, // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Adjust the allowed methods as needed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Adjust the allowed headers as needed
+    credentials: true, // Allow credentials like cookies, if applicable
+    preflightContinue: false, // Set this to true if you want to handle preflight requests explicitly
+    optionsSuccessStatus: 200, // Set the response status code for successful OPTIONS requests
+  });
   await app.listen(process.env.PORT || 3001);
-
 }
 bootstrap();
-
-
