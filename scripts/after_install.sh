d6027eafc03960543f44332b34456a5b7c024ab9
Linal180 <amirnisar75@gmail.com>
2023-07-26 16:47:06 +0500

    commented after install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-# directory name
+# # directory name
 
-PRO_DIR="/home/education-backend"
+# PRO_DIR="/home/education-backend"
 
-# give permission for everything in the app directory
-sudo chmod -R 777 "$PRO_DIR"
+# # give permission for everything in the app directory
+# sudo chmod -R 777 "$PRO_DIR"
 
-# add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in path now)
+# # add npm and node to path
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in path now)
 
-nvm use 18.12.1
+# nvm use 18.12.1
 
-# install node modules
-cd $PRO_DIR
-npm install
+# # install node modules
+# cd "${PRO_DIR}"
+# # npm install
+# # npm run build
