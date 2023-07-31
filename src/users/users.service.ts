b2076116432e1ec45341a3b32dc8740272815b74
Linal180 <amirnisar75@gmail.com>
2023-07-31 16:35:54 +0500

    check role of cognitoUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -370,11 +370,12 @@ export class UsersService {
 
       if (cognitoUser) {
         const { accessToken } = await this.cognitoService.loginUser({ username: cognitoUser.Username } as User, password)
+        const role = this.cognitoService.getAwsUserRole({ User: cognitoUser } as AdminCreateUserCommandOutput);
 
         if (accessToken) {
           return {
             email,
-            roles: []
+            roles: [{role: role }] as Role[]
           };
         }
       }
