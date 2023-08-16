f36b781648139f654262e9764ccdff3c7fbeb149
Linal180 <amirnisar75@gmail.com>
2023-08-16 12:13:54 +0500

    added comment in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
-  app.enableCors(); // Enable CORS for all requests ...Khalid on staging branch....
+  app.enableCors(); // Enable CORS for all requests ...Khalid testing..
   await app.listen(port);
 }
 bootstrap();
