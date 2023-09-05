f4031d0de2e568e1969034b56e4837fb999ffce8
Linal180 <amirnisar75@gmail.com>
2023-09-05 18:01:32 +0500

    Delete user webhook updated

--- src/users/api.user.controller.ts
+++ src/users/api.user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Param, Post, Query } from "@nestjs/common";
+import { Controller, Delete, HttpException, HttpStatus, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UpdateUserEmailInput } from "src/util/interfaces";
 
@@ -7,22 +7,28 @@ export class CustomUserController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post('/users/update-email')
-  async updateUserEmail(  @Query('userName') userName: string, @Query('newEmail') newEmail: string): Promise<any>{
+  async updateUserEmail(@Query('userName') userName: string, @Query('newEmail') newEmail: string): Promise<any> {
     // Validate and process the request here
     if (!userName || !newEmail) {
       throw new HttpException('Missing userName or newEmail parameter', HttpStatus.BAD_REQUEST);
     }
-    return {
-      response :  await this.usersService.updateByEmail({userName, newEmail} as UpdateUserEmailInput) ? { status: 200, message: "User Email update Successfully" } : { status: 200, message: "User Not Found"  }
-    }
 
+    return {
+      response:
+        await this.usersService.updateByEmail({ userName, newEmail } as UpdateUserEmailInput)
+          ? { status: 200, message: "User Email update Successfully" }
+          : { status: 200, message: "User Not Found" }
+    };
   }
 
-  @Post('delete-user')
-  async deleteUser(@Query('username') username:string): Promise<any> {
+  @Delete('delete-user')
+  async deleteUser(@Query('username') username: string): Promise<any> {
     return {
-      response: await this.usersService.deleteUserOnEntityField('username' , username) ? { status: 200, message: "User Delete Successfully" } : { status: 200, message: "User Not Found" }
-    }
+      response:
+        await this.usersService.deleteUserOnEntityField('username', username)
+          ? { status: 200, message: "User Delete Successfully" }
+          : { status: 200, message: "User Not Found" }
+    };
   }
 
 }
