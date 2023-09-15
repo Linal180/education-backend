4d17511003310d4ece51dfbef693c3b22c0f7105
Linal180 <amirnisar75@gmail.com>
2023-09-15 16:59:04 +0500

    deploying to prod

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
-  app.enableCors(); // Enable CORS for all requests ...Khalid made changes in master...
+  app.enableCors(); // Enable CORS for all requests ...Khalid made changes in master.....
   await app.listen(port);
 }
 bootstrap();
